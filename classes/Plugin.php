bce0008f5c30eda7bae4fe53eed87f4a5367f86b
Bryce Meyer <bryce@axolotlinteractive.com>
2015-05-30 16:53:21 -0500

    created plugin file

---
+++ classes/Plugin.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 5/30/15
+ * Time: 4:50 PM
+ */
+
+namespace AboutUs;
+
+
+use WordWrap\LifeCycle;
+
+class Plugin extends LifeCycle{
+
+    /**
+     * See: http://plugin.michael-simpson.com/?page_id=31
+     * @return array of option meta data.
+     */
+    public function getOptionMetaData() {
+        //  http://plugin.michael-simpson.com/?page_id=31
+        return array(
+            //'_version' => array('Installed Version'), // Leave this one commented-out. Uncomment to test upgrades.
+            'ATextInput' => array(__('Enter in some text', 'my-awesome-plugin')),
+            'Donated' => array(__('I have donated to this plugin', 'my-awesome-plugin'), 'false', 'true'),
+            'CanSeeSubmitData' => array(__('Can See Submission data', 'my-awesome-plugin'),
+                'Administrator', 'Editor', 'Author', 'Contributor', 'Subscriber', 'Anyone')
+        );
+    }
+
+//    protected function getOptionValueI18nString($optionValue) {
+//        $i18nValue = parent::getOptionValueI18nString($optionValue);
+//        return $i18nValue;
+//    }
+
+    protected function initOptions() {
+        $options = $this->getOptionMetaData();
+        if (!empty($options)) {
+            foreach ($options as $key => $arr) {
+                if (is_array($arr) && count($arr > 1)) {
+                    $this->addOption($key, $arr[1]);
+                }
+            }
+        }
+    }
+
+    public function getPluginDisplayName() {
+        return 'About Us';
+    }
+
+    protected function getMainPluginFileName() {
+        return 'about-us.php';
+    }
+
+    /**
+     * See: http://plugin.michael-simpson.com/?page_id=101
+     * Called by install() to create any database tables if needed.
+     * Best Practice:
+     * (1) Prefix all table names with $wpdb->prefix
+     * (2) make table names lower case only
+     * @return void
+     */
+    protected function installDatabaseTables() {
+
+    }
+
+    /**
+     * See: http://plugin.michael-simpson.com/?page_id=101
+     * Drop plugin-created tables on uninstall.
+     * @return void
+     */
+    protected function unInstallDatabaseTables() {
+
+    }
+
+
+    /**
+     * Perform actions when upgrading from version X to version Y
+     * See: http://plugin.michael-simpson.com/?page_id=35
+     * @return void
+     */
+    public function upgrade() {
+
+    }
+
+    public function addActionsAndFilters() {
+
+
+    }
+}
