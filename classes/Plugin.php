55d758092c3910dbe2dc50fe9dc2f87b4c9cd0ce
Bryce Meyer <bryce@axolotlinteractive.com>
2015-06-13 13:37:14 -0500

    registered html asset path

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -86,6 +86,8 @@ class Plugin extends LifeCycle{
 
     public function addActionsAndFilters() {
 
+        $this->assetManager->registerAssetType("html", __DIR__ . "/../assets/html/");
+
         $sc = new AboutUsShortCode($this);
         $sc->register('about_us');
 
